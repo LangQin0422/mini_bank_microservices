56fdcbbff83bf41c0ccb344193ca73e0eca27891
LaaaaaQ <petertsing@outlook.com>
2024-03-04 16:29:45 -0500

    3/4/2024

---
+++ section2/loans/src/main/java/com/eazybytes/loans/constants/LoansConstants.java
@@ -0,0 +1,27 @@
+package com.eazybytes.loans.constants;
+
+public final class LoansConstants {
+
+    private LoansConstants() {
+    }
+
+    public static final String LOAN_TYPE_HOME = "Home Loan";
+
+    public static final int NEW_LOAN_LIMIT = 1_00_000;
+
+    public static final String STATUS_200 = "200";
+
+    public static final String MESSAGE_200 = "Request processed successfully";
+
+    public static final String STATUS_201 = "201";
+
+    public static final String MESSAGE_201 = "New loan created successfully";
+
+    public static final String STATUS_417 = "417";
+
+    public static final String MESSAGE_417 = "Update operation failed. Please try again or contact Dev team";
+
+    public static final String STATUS_500 = "500";
+
+    public static final String MESSAGE_500 = "Internal Server Error";
+}
